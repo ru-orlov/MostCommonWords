285f79ea92ab76534271fc815299147fb1a35c7e
ruorlov@gmail.com <#vjlegj,tk#1982>
2017-08-15 19:03:00 +0200

    [GUI] - impl the dropdowns and lists

--- app/src/main/java/mostcommonwords/ruorlov/ru/mostcommonwords/MainActivity.java
+++ app/src/main/java/mostcommonwords/ruorlov/ru/mostcommonwords/MainActivity.java
@@ -3,23 +3,30 @@ package mostcommonwords.ruorlov.ru.mostcommonwords;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import mostcommonwords.ruorlov.ru.mostcommonwords.tools.InstallService;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private String[] arrayLang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         startService(new Intent(MainActivity.this, InstallService.class));
-
         setContentView(R.layout.activity_main);
 
-        System.out.println("MainActivity >>>>");
+        final Spinner leftSpinner = (Spinner)findViewById(R.id.left_lang);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, initLangArray());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        leftSpinner.setAdapter(adapter);
+    }
 
+    private String[] initLangArray(){
+        return this.arrayLang = new String[] {
+                "English", "German", "French", "Italian", "Polish", "Russian"
+        };
     }
 }
